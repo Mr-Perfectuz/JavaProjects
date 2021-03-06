d1e80ae0e4de3e06bd345694c11f485b73833283
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-06 15:04:15 +0500

    Expressions.java has been updated

---
+++ src/Expressions.java
@@ -0,0 +1,15 @@
+
+public class Expressions {
+    public static void main(String[] arg){
+        // expressions = operands & operations
+        //operands = values, variables, numbers, quantity
+        //operations = + - / *
+
+        double friends = 10;
+        friends = friends / 3;
+
+        System.out.println(friends);
+
+
+    }
+}
