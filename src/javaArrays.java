484fbcc4f43d4ad7236cb1e3b55b85a9592557c6
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-07 21:05:11 +0500

    javaArrays.java has been implemented

--- src/javaArrays.java
+++ src/javaArrays.java
@@ -1,14 +1,13 @@
 public class javaArrays {
     public static void main(String[] args){
 //        String[] cars = {"Ferrari","Tesla","BMW"};
-        int[] numbers = {1, 3, 4};
+        int[] numbers = {1, 3, 4, 2};
         String[] cars = new String[4];
 
         cars[0] = "Ferrari";
         cars[1] = "Porsce";
         cars[2] = "Tesla";
         cars[3] = "Gm";
-
 
         for (int i=0; i<cars.length; i++){
             System.out.println(cars[i]);
