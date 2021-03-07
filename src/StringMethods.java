2cb5d859a2030efe2b6d95e18c55827b9e7d23b4
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-07 22:12:51 +0500

    StringMethods.java has been implemented

---
+++ src/StringMethods.java
@@ -0,0 +1,9 @@
+public class StringMethods {
+    public static void main(String[] args){
+        String name = "Tony";
+
+//        boolean result = name.equalsIgnoreCase("tony");
+
+        System.out.println(result);
+    }
+}
