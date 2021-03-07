ca95091f421809c2f1cf795389e424f2c9621c21
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-07 20:00:27 +0500

    forLoop.java has been implemented

--- src/forLoop.java
+++ src/forLoop.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class forLoop {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String name1, name2, name3;
+        System.out.println("Enter the name1");
+        name1 =  scanner.next();
 
-        for (int i=10; i>=0; i--){
+        System.out.println("Enter the name2");
+        name2 =  scanner.next();
+
+        System.out.println("Enter the name3");
+        name3 =  scanner.next();
+
+        for (int i=0; i<=3; i++){
+
             System.out.println(i);
         }
-        System.out.println("Hello guys");
+        System.out.println("Hello " + name1);
+        System.out.println("Hello " + name2);
+        System.out.println("Hello " + name3);
 
     }
 }
