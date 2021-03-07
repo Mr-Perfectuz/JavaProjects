e0f0ae6b66e924ce78cdf4d022412d93c52b589c
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-07 22:17:28 +0500

    StringMethods.java has been implemented

--- src/StringMethods.java
+++ src/StringMethods.java
@@ -3,6 +3,14 @@ public class StringMethods {
         String name = "Tony";
 
 //        boolean result = name.equalsIgnoreCase("tony");
+//        boolean result = name.isEmpty();
+//        char result = name.charAt(0);
+//        int result = name.indexOf("T");
+//        String  result = name.toLowerCase();
+//        String result = name.toUpperCase();
+//        String result = name.trim();
+//        String result = name.repeat(4);
+        String result = name.replace('o', 'a');
 
         System.out.println(result);
     }
