2b81ae3268b47b2ae7c555e55220bdf61c69d45f
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-07 19:52:16 +0500

    forLoop.java has been implemented

--- src/forLoop.java
+++ src/forLoop.java
@@ -1,2 +1,10 @@
 public class forLoop {
+    public static void main(String[] args){
+
+        for (int i=10; i>=0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Hello guys");
+
+    }
 }
