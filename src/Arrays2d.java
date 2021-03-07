321ffdbe78a26f9c545c0d4049c20f2021a90122
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-07 21:25:45 +0500

    Arrays2d.java has been implemented

--- src/Arrays2d.java
+++ src/Arrays2d.java
@@ -1,15 +1,18 @@
 public class Arrays2d {
     public static void main(String [] args){
-        String[][] names = new String[3][3];
-        names[0][0] = "Row1";
-        names[0][1] ="Row2";
-        names[0][2] ="Row3";
-        names[1][0] ="Row4";
-        names[1][1] ="Row5";
-        names[1][2] ="Row6";
-        names[2][0] ="Row7";
-        names[2][1] ="Row8";
-        names[2][2] ="Row9";
+
+        String [][] names = {{"Row1", "Row2", "Row3"},{"Row4","Row5","Row6"},{"Row7","Row8","Row9"}};
+
+//        String[][] names = new String[3][3];
+//        names[0][0] = "Row1";
+//        names[0][1] ="Row2";
+//        names[0][2] ="Row3";
+//        names[1][0] ="Row4";
+//        names[1][1] ="Row5";
+//        names[1][2] ="Row6";
+//        names[2][0] ="Row7";
+//        names[2][1] ="Row8";
+//        names[2][2] ="Row9";
 
         for (int i=0; i<names.length; i++) {
             System.out.println();
