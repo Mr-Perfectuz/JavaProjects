da0558fff9506b4f9fcf0e3a6b475062c3004886
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-10 20:51:30 +0500

    arrayList2D.java has been implemented

---
+++ src/arrayList2D.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class arrayList2D {
+    public static void main(String[] args){
+
+        ArrayList<String> bakeryLIst = new ArrayList<String>();
+        bakeryLIst.add("Pasta");
+        bakeryLIst.add("Garlic Bread");
+        bakeryLIst.add("Donuts");
+
+        ArrayList<String> ProduceLIst = new ArrayList<String>();
+        ProduceLIst.add("tometos");
+        ProduceLIst.add("zukinich");
+        ProduceLIst.add("Pappers");
+
+        ArrayList<String> DrinksLIst = new ArrayList<String>();
+        DrinksLIst.add("Cola");
+        DrinksLIst.add("Coffe");
+
+        System.out.println(ProduceLIst);
+    }
+}
