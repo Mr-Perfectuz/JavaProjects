7dc47d35662b12a60528334c51a90697dd219dae
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-10 20:42:13 +0500

    arrayList.java has been implemented

--- src/arrayList.java
+++ src/arrayList.java
@@ -1,7 +1,25 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class arrayList {
     public static void main(String[] args){
         ArrayList<String> food = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+
+
+//        System.out.println("What do you order ?");
+
+        food.add("Pizza");
+        food.add("Hamburger");
+        food.add("Hotdog");
+
+
+        food.set(0, "Sushi");
+        food.remove(2);
+//        food.clear();
+
+        for (int i=0; i<food.size(); i++){
+            System.out.println("The list of food is "+ food.get(i));
+        }
     }
 }
