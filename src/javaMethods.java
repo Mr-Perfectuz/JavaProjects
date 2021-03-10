b5d5fbc5ee89833640d6ef80d3d88f7695f26f1d
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-11 00:02:21 +0500

    javaMethods.java has been implemented

--- src/javaMethods.java
+++ src/javaMethods.java
@@ -6,17 +6,33 @@ public class javaMethods {
 //        method  is a block of code that is executed when it is called upon
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is your name ? ");
-        String name = scanner.nextLine();
+        int num1;
+        int num2;
+
+        System.out.println("Enter number one: ");
+        num1 = scanner.nextInt();
+
+        System.out.println("Enter number two: ");
+        num2 = scanner.nextInt();
 
 
-        System.out.println("What is your age ? ");
-        int age = scanner.nextInt();
+//        System.out.println("What is your name ? ");
+//        String name = scanner.next();
+//
+//        System.out.println("What is your age ? ");
+//        int age = scanner.nextInt();
 
-        hello(name, age);
+//        hello(name, age);
+        int z = sum(num1, num2);
+        System.out.println("The sum of "+ num1 +" and "+ num2+" is "+ z);
     }
 
    static void hello(String name, int age){
         System.out.println("Hello "+ name+", I am "+age+" years old");
+    }
+
+    static int sum(int num1, int num2){
+        int add = num1 + num2;
+        return add;
     }
 }
