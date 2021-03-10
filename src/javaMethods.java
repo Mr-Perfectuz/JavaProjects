f6197196239880e99e4246267f6132b988862d6c
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-10 22:59:47 +0500

    javaMethods.java has been implemented

---
+++ src/javaMethods.java
@@ -0,0 +1,12 @@
+public class javaMethods {
+    public static void main(String[] args){
+
+//        method  is a block of code that is executed when it is called upon
+
+        hello();
+    }
+
+   static void hello(){
+        System.out.println("Hello Professor");
+    }
+}
