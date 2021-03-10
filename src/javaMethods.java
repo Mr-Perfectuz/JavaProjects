c6b062ed4c49e21791746872b8ba1210fb819249
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-10 23:02:39 +0500

    javaMethods.java has been implemented

--- src/javaMethods.java
+++ src/javaMethods.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class javaMethods {
     public static void main(String[] args){
 
 //        method  is a block of code that is executed when it is called upon
+        Scanner scanner = new Scanner(System.in);
 
-        hello();
+        System.out.println("What is your name ? ");
+        String name = scanner.nextLine();
+
+        hello(name);
     }
 
-   static void hello(){
-        System.out.println("Hello Professor");
+   static void hello(String name){
+        System.out.println("Hello "+ name);
     }
 }
