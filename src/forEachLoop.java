a5c5c20404595f4528f500f530cc48b3c919ae05
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-10 22:49:35 +0500

    forEachLoop.java has been implemented

--- src/forEachLoop.java
+++ src/forEachLoop.java
@@ -4,13 +4,19 @@ public class forEachLoop {
     public  static  void main(String[] args){
 //        String[] animals = {"cat","dog","cow","horse"};
 
-        ArrayList<String> animals = new ArrayList<>();
-        animals.add("cat");
-        animals.add("dog");
-        animals.add("cow");
-        animals.add("horse");
+//        ArrayList<String> animals = new ArrayList<>();
+//        animals.add("cat");
+//        animals.add("dog");
+//        animals.add("cow");
+//        animals.add("horse");
 
-        for (String i: animals){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        for (Integer i: numbers){
             System.out.println(i);
 
         }
