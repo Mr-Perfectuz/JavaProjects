71323b371b64fd5b289e70c1c549d5d2bf0e8f7d
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-11 10:58:59 +0500

    overloadedMethods.java has been implemented

--- src/calculateRadius.java
+++ src/calculateRadius.java
@@ -1,29 +1,47 @@
-
+import java.math.*;
 import java.util.Scanner;
 
 public class calculateRadius {
     public static void main(String[] args){
      Scanner scanner = new Scanner(System.in);
-     int num1, num2;
+        double Fahrenheit;
 
-        System.out.println("Enter the first number: ");
-        num1 = scanner.nextInt();
+        System.out.println(" Enter a degree in Fahrenheit: ");
+        Fahrenheit = scanner.nextDouble();
 
-        System.out.println("Enter the second number: ");
-        num2= scanner.nextInt();
+        double Celsius =  (Fahrenheit - 32) *5/9 ;
 
-        int add = num1 + num2;
-        int substract = num1 - num2;
-        int multiple = num1 * num2;
-        int quotient = num1 / num2;
 
-        int reminder = 5%6;
+        System.out.println("Fahrenheit "+ Fahrenheit+ " is "+ Celsius+ " in Celsius ");
 
-        System.out.println(num1 + " + "+ num2+ " = "+ add);
-        System.out.println(num1 + " - "+ num2+ " = "+ substract);
-        System.out.println(num1 + " * "+ num2+ " = "+ multiple);
-        System.out.println(num1 + " / "+ num2+ " = "+" the quotient is "+ quotient+ " with a reminder of "+ reminder);
 
+
+
+
+
+
+
+
+//     int num1, num2;
+//
+//        System.out.println("Enter the first number: ");
+//        num1 = scanner.nextInt();
+//
+//        System.out.println("Enter the second number: ");
+//        num2= scanner.nextInt();
+//
+//        int add = num1 + num2;
+//        int substract = num1 - num2;
+//        int multiple = num1 * num2;
+//        int quotient = num1 / num2;
+//
+//        int reminder = 5%6;
+//
+//        System.out.println(num1 + " + "+ num2+ " = "+ add);
+//        System.out.println(num1 + " - "+ num2+ " = "+ substract);
+//        System.out.println(num1 + " * "+ num2+ " = "+ multiple);
+//        System.out.println(num1 + " / "+ num2+ " = "+" the quotient is "+ quotient+ " with a reminder of "+ reminder);
+//
 
 
 //        System.out.println("Enter the first number: ");
