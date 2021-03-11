cc99444966b8a6c2b6ddcaa2fd83f8faf2aae4ac
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-11 11:32:59 +0500

    overloadedMethods.java has been implemented

--- src/calculateRadius.java
+++ src/calculateRadius.java
@@ -1,18 +1,57 @@
 import java.math.*;
+import java.util.Date;
 import java.util.Scanner;
 
 public class calculateRadius {
     public static void main(String[] args){
      Scanner scanner = new Scanner(System.in);
-        double Fahrenheit;
 
-        System.out.println(" Enter a degree in Fahrenheit: ");
-        Fahrenheit = scanner.nextDouble();
-
-        double Celsius =  (Fahrenheit - 32) *5/9 ;
+//        Date date=java.util.Calendar.getInstance().getTime();
+//        System.out.println(date);
 
 
-        System.out.println("Fahrenheit "+ Fahrenheit+ " is "+ Celsius+ " in Celsius ");
+        long totalMillisecond = System.currentTimeMillis();
+        long totalSeconds = totalMillisecond  / 1000;
+        long currentSeconds = totalSeconds % 60;
+        long totalMinutes = totalSeconds  / 60;
+        long currentMinutes = totalMinutes % 60;
+        long totalHours = totalMinutes / 60;
+        long currentHours = totalHours % 24;
+        long GMT = currentHours + 5;
+        long GMT1 = currentHours + 9;
+
+
+        System.out.println("Current time is "+ currentHours+ " hour "+ currentMinutes+ " minutes "+ currentSeconds+" seconds");
+        System.out.println("Current time(GMT + 9): "+ GMT1 + ":" + currentMinutes+ ":"+ currentSeconds);
+        System.out.println("Current time(GMT + 5): "+ GMT+ ":" + currentMinutes+ ":"+ currentSeconds);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     double f= 100;
+//     double c = (f-32) / 1.8;
+//        System.out.println(c);
+
+//        double Fahrenheit;
+//
+//        System.out.println(" Enter a degree in Fahrenheit: ");
+//        Fahrenheit = scanner.nextDouble();
+//
+//        double Celsius =  (Fahrenheit - 32) *5/9 ;
+//
+//
+//        System.out.println("Fahrenheit "+ Fahrenheit+ " is "+ Celsius+ " in Celsius ");
 
 
 
