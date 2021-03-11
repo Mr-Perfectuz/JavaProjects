8ee61714f73b2cfb9e6b54983c74f5942410137a
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-11 10:16:59 +0500

    overloadedMethods.java has been implemented

--- src/calculateRadius.java
+++ src/calculateRadius.java
@@ -4,19 +4,39 @@ import java.util.Scanner;
 public class calculateRadius {
     public static void main(String[] args){
      Scanner scanner = new Scanner(System.in);
-     double num1, num2, num3;
+     int num1, num2;
 
         System.out.println("Enter the first number: ");
-        num1 = scanner.nextDouble();
+        num1 = scanner.nextInt();
 
-        System.out.println("Enter the second  number: ");
-        num2 = scanner.nextDouble();
+        System.out.println("Enter the second number: ");
+        num2= scanner.nextInt();
 
-        System.out.println("Enter the third number: ");
-        num3 = scanner.nextDouble();
+        int add = num1 + num2;
+        int substract = num1 - num2;
+        int multiple = num1 * num2;
+        int quotient = num1 / num2;
 
-        double averageNumber = (num1 + num2 + num3) /3;
-        System.out.println(" The average of " + num1 + " "+ num2 +" and "+ num3+ " is "+ averageNumber);
+        int reminder = 5%6;
+
+        System.out.println(num1 + " + "+ num2+ " = "+ add);
+        System.out.println(num1 + " - "+ num2+ " = "+ substract);
+        System.out.println(num1 + " * "+ num2+ " = "+ multiple);
+        System.out.println(num1 + " / "+ num2+ " = "+" the quotient is "+ quotient+ " with a reminder of "+ reminder);
+
+
+
+//        System.out.println("Enter the first number: ");
+//        num1 = scanner.nextDouble();
+//
+//        System.out.println("Enter the second  number: ");
+//        num2 = scanner.nextDouble();
+//
+//        System.out.println("Enter the third number: ");
+//        num3 = scanner.nextDouble();
+//
+//        double averageNumber = (num1 + num2 + num3) /3;
+//        System.out.println(" The average of " + num1 + " "+ num2 +" and "+ num3+ " is "+ averageNumber);
 
     }
 }
