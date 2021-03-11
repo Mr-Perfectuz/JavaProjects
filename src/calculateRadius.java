7b0dd85619944d0f93fee467e1e13c309168fd29
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-11 09:51:03 +0500

    overloadedMethods.java has been implemented

--- src/calculateRadius.java
+++ src/calculateRadius.java
@@ -1,15 +1,22 @@
+
 import java.util.Scanner;
 
 public class calculateRadius {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+     Scanner scanner = new Scanner(System.in);
+     double num1, num2, num3;
 
-        System.out.println(" Enter the Radius of the circle: ");
-        int radius = scanner.nextInt();
+        System.out.println("Enter the first number: ");
+        num1 = scanner.nextDouble();
 
-        double area = radius * radius * 3.14;
+        System.out.println("Enter the second  number: ");
+        num2 = scanner.nextDouble();
 
-        System.out.println(" The area of the circle if "+ area);
+        System.out.println("Enter the third number: ");
+        num3 = scanner.nextDouble();
+
+        double averageNumber = (num1 + num2 + num3) /3;
+        System.out.println(" The average of " + num1 + " "+ num2 +" and "+ num3+ " is "+ averageNumber);
 
     }
 }
