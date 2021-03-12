18f918403561e1b1a438b25d29c815ab9e2aaf85
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-12 23:12:47 +0500

    printFMethod.java has been implemented

--- src/printFMethod.java
+++ src/printFMethod.java
@@ -3,6 +3,7 @@ public class printFMethod {
         // printf= an optional method to control, format, and display text to the console window
         // two arguments = format string + (object/variable/value)
 
+        System.out.printf(" This is %d a message ", 123);
 
     }
 }
