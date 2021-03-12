3e19ec17d4814ec0f8d407a1afa5f6c4b7a1a336
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-12 23:22:10 +0500

    printFMethod.java has been implemented

--- src/printFMethod.java
+++ src/printFMethod.java
@@ -3,7 +3,19 @@ public class printFMethod {
         // printf= an optional method to control, format, and display text to the console window
         // two arguments = format string + (object/variable/value)
 
-        System.out.printf(" This is %d a message ", 123);
+//        System.out.printf(" This is %d a message ", 123);
+
+        boolean myBoolean = true;
+        char myChar = '@';
+        String myString = "Joe";
+        int myInt = 50;
+        double myDouble = 1000;
+
+//        System.out.printf( "%b", myBoolean);
+//        System.out.printf("%c", myChar);
+//        System.out.printf("%s", myString);
+//        System.out.printf("%d", myInt);
+        System.out.printf("%f", myDouble);
 
     }
 }
