1cafeed86d81a7b698438db07e3bbeb535df6f0c
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-03-13 10:15:55 +0500

    printFMethod.java has been implemented

--- src/printFMethod.java
+++ src/printFMethod.java
@@ -11,6 +11,15 @@ public class printFMethod {
         int myInt = 50;
         double myDouble = 1000;
 
+        //[Width]
+        // minimum number of characters to be written as an output
+//        System.out.printf(" Hello %10s", myString);
+
+
+//         [precision]
+        // sets number of digits of precision when outputting floating- point values.
+        System.out.printf(" You have this much money ");
+
 //        System.out.printf( "%b", myBoolean);
 //        System.out.printf("%c", myChar);
 //        System.out.printf("%s", myString);
